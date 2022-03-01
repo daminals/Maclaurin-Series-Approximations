a79c5f9d5b7abf921a548c3fdec9b1ae690a840b
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-03-01 01:55:53 -0500

    taylor approximation accurate to 11 digits

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+// taylor
+// Daniel Kogan
+// 02.28.2022
+
+
+use statrs::function::factorial::factorial;
+
 fn main() {
-    sin(5.0);
+    sin(7.0);
 }
 
 fn sin(x: f64) {
@@ -8,17 +15,18 @@ fn sin(x: f64) {
     // (2n+1)!
     let mut n = 0;
     let mut sum: f64 = 0.0;
-    while n<9 {
+    while n<150 { // goes up to 19 factorial before overflow
         let negative_1: i32 = -1;
         let two_n_plus_1: f64 = (*&n  * 2 + 1) as f64;
-        sum += ((negative_1.pow(*&n) as f64 ) * (*&x).powf(*&two_n_plus_1))/(factorial(two_n_plus_1 as u128) as f64);
-        //print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // clear terminal
+        sum += ((negative_1.pow(*&n) as f64 ) * (*&x).powf(*&two_n_plus_1))/(factorial(two_n_plus_1 as u64) as f64);
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // clear terminal
         println!("{}", sum);
         n+=1;
     }    
-
 }
 
+/* statrs factorial allows for up to 170! before overflow
+this fn only allows 21!
 fn factorial (num: u128) -> u128 {
     match num {
         0 => 1,
@@ -26,3 +34,4 @@ fn factorial (num: u128) -> u128 {
         _ => factorial(num - 1) * num,
     }
 }
+*/
